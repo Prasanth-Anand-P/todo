5ca0bb8bbed0e9cbde6b2a0748692e43256f4d2e
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-08-16 00:11:47 +0530

    Updated urls.py

--- task/urls.py
+++ task/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('task_update/<str:pk>/', views.taskUpdate, name = 'task_update'),
+    path('task_delete/<str:pk>/', views.taskDelete, name = 'task_delete'),
+    
 
 ]
